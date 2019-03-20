44b25a209177f15bc3baa013f1b9664b53ab94bd
Vlad Razin <47825278+Klautts@users.noreply.github.com>
2019-03-21 02:42:13 +0300

    add task advanced zeros 98%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+	var sum = 0;
+	var main ;
+	var newsum;
+	var newmain
+	var k = 0;
+	for(var i = 2; i <= base; i++){
+		for(var j=2; j <= i; j++){
+			if(i % j === 0) break;
+		}
+		if(j === i && base % i === 0){
+			main = i;
+		}
+	}
+	for(var r=1; Math.pow(main,r) <= number; r++){
+		sum = Math.floor(number/Math.pow(main,r)) + sum;
+	}
+	while(base % main === 0){
+		base=base/main;
+		k++;
+	}
+	if(k > 1){
+		return Math.floor(newsum = sum/k);
+	}
+	else{
+		return sum;	
+	}
 }
